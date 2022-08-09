4556172708d260d3fd3ac81cee95b546845148c4
Trilliant <mrzeero0@mail.ru>
2022-08-09 15:31:17 +0300

    refactored

--- src/logger/Solution.java
+++ src/logger/Solution.java
@@ -8,8 +8,8 @@ public class Solution {
 
     }
     public static void logging() {
-    final Logger LOGGER = Logger.getLogger("com.javamentor.logging.Test");
-    LOGGER.log(Level.INFO, "Все хорошо");
-    LOGGER.log(Level.WARNING, "Произошла ошибка");
+    final Logger logger = Logger.getLogger("com.javamentor.logging.Test");
+    logger.log(Level.INFO, "Все хорошо");
+    logger.log(Level.WARNING, "Произошла ошибка");
     }
 }
