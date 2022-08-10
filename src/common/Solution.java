67e7cccb0d346e588478b970dbced6c22a84b554
trilliant <mrzeero0@mail.ru>
2022-08-10 22:35:47 +0300

    used for UnaryOperator

--- src/common/Solution.java
+++ src/common/Solution.java
@@ -1,23 +1,15 @@
 package common;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.util.function.UnaryOperator;
 
 public class Solution {
 
     public static void main(String[] args) {
-        byte[] bytes = {48, 49, 50, 51};
-        InputStream inputStream = new ByteArrayInputStream(bytes);
-        try {
-            System.out.println(readAsString(inputStream, StandardCharsets.US_ASCII));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println(sqrt());
     }
 
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-    return new String(inputStream.readAllBytes(), charset);
+    public static UnaryOperator<Integer> sqrt() {
+        return (x) -> x * x;
     }
 }
 
